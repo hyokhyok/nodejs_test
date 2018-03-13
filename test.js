bb00400ca4968b9b3ff50a981f94df2934895716
hyokhyok <[hyogilee257@gmail.com]>
2018-03-13 17:58:34 +0900

    sync 해결 - python path 설정 변경

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
-console.log("hello nodejs");
 var steem = require("steem")
 steem.api.setOptions({url: 'https://api.steemit.com'});
-steem.api.getAccounts(['ned', 'dan'], function(err, response){
-    console.log(err, response);
-});
-console.log("goodbye")
+
+var sync = require('synchronize');
+var fiber = sync.fiber;
+var await = sync.await;
+var defer = sync.defer;
+try {
+    fiber(function() {
+        var obj = await(steem.api.getAccounts(['ned', 'dan'], defer()));
+        console.log(obj);
+        obj = await(steem.api.getAccounts(['nhj12311'], defer()));
+        console.log(obj);
+    });
+} catch(err) {
+  console.log(err);
+}
+console.log("goodbye 실화냐")
